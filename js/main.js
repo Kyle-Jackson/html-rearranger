429189a28c69f2468312edbcc9c5c220bbb8bf35
Kyle-Jackson <kylejackson@clare-pc.gateway.2wire.net>
2016-03-17 02:04:31 -0400

    Got the below ground veggies header to show.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-var $h1abvgrnd = $('<h1>');
-var $h1blwgrnd = $('<h1>');
-var $ulabv = $('<h1>');
-var $ulbl = $('<h1>');
 
-$h1abvgrnd.html('Below ground veggies');
-$h1blwgrnd.html('Above ground veggies');
-$ulabv.addClass('veggies-list');
+var $h1above = $('<h1>');
+var $h1below = $('<h1>');
+var $ulabove = $('<ul>');
+var $ulbelow = $('<ul>');
 
+$h1above.html('Below ground veggies');
+$h1below.html('Above ground veggies');
 
+$('body').append($h1above);
+$('body').append($u1above);
 
-veggies.forEach(function (veggies) {
-  var $li = $('<li>');
+$('body').append($h1below);
+$('body').append($ulbelow);
 
+$('ul li').each(function() {
+  if($(this).hasClass('above')){
+    $ulabove.append($(this))
+  } else {
+    $ulbelow.append($(this))
+  }
 });
